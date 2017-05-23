e28155ac181ad4b3dc8a666a71efad58bfb39dfb
FMCalisto <FMCalisto@users.noreply.github.com>
2017-05-23 14:52:11 +0100

    [CREATE] 3 Deploy Servers

---
+++ src/js/dicomImage.js
@@ -0,0 +1,86 @@
+var element = $('#dicomImage').get(0);
+var currentImage = 1;
+var imageId = 'example://' + currentImage;
+
+$(document).ready(function() {
+    var element = $('#dicomImage').get(0);
+    cornerstone.enable(element);
+    var imageIds = [
+        'example://1',
+        'example://2',
+        'example://3'
+    ];
+    var currentImageIndex = 0;
+    // show image #1 initially
+    function updateTheImage(imageIndex) {
+        currentImageIndex = imageIndex;
+        cornerstoneTools.toolStyle.setToolWidth(3);
+        cornerstoneTools.toolColors.setToolColor("#ffcc33");
+        cornerstoneTools.toolColors.setActiveColor("#0099ff");
+        cornerstoneTools.toolColors.setFillColor("#0099ff");
+        // image enable the dicomImage element
+        cornerstone.enable(element);
+        cornerstone.loadImage(imageIds[currentImageIndex]).then(function(image) {
+            cornerstone.displayImage(element, image);
+            cornerstoneTools.mouseInput.enable(element);
+            // Enable all tools we want to use with this element
+            cornerstoneTools.freehand.activate(element, 1);
+            activate("#activate");
+            function activate(id)
+            {
+                $('a').removeClass('active');
+                $(id).addClass('active');
+            }
+            // Tool button event handlers that set the new active tool
+            $('#disable').click(function() {
+                activate("#disable");
+                cornerstoneTools.freehand.disable(element);
+                return false;
+            });
+            $('#enable').click(function() {
+                activate("#enable");
+                cornerstoneTools.freehand.enable(element);
+                return false;
+            });
+            $('#activate').click(function() {
+                activate("#activate");
+                cornerstoneTools.freehand.activate(element, 1);
+                return false;
+            });
+            $('#deactivate').click(function() {
+                activate("#deactivate");
+                cornerstoneTools.freehand.deactivate(element, 1);
+                return false;
+            });
+            $('#clearToolData').click(function() {
+                var toolStateManager = cornerstoneTools.getElementToolStateManager(element);
+                // Note that this only works on ImageId-specific tool state managers (for now)
+                toolStateManager.clear(element)
+                cornerstone.updateImage(element);
+            });
+        });
+    };
+
+    updateTheImage(0);
+
+    $('#imageButtonBack').click(function (e) {
+        if (currentImageIndex == 2) {
+            updateTheImage(1);
+        } else if (currentImageIndex == 1) {
+            updateTheImage(0);
+        } else {
+            updateTheImage(0);
+        }
+    });
+
+    $('#imageButtonNext').click(function (e) {
+        if (currentImageIndex == 0) {
+            updateTheImage(1);
+        } else if (currentImageIndex == 1) {
+            updateTheImage(2);
+        } else {
+            updateTheImage(2);
+        }
+    });
+
+});
