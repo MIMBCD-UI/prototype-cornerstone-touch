5b7cb6f0235a2b21c8ea131610aea14b8a6dd519
FMCalisto <FMCalisto@users.noreply.github.com>
2017-06-14 09:03:44 +0100

    [DELETE] Servers

--- servers/server_2/js/mouseflow.js
+++
@@ -1,7 +0,0 @@
-window._mfq = window._mfq || [];
-(function() {
-	var mf = document.createElement("script");
-	mf.type = "text/javascript"; mf.async = true;
-	mf.src = "//cdn.mouseflow.com/projects/944e7bb9-8e31-49d0-9157-aab954ad3319.js";
-	document.getElementsByTagName("head")[0].appendChild(mf);
-})();
